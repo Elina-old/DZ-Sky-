b0893192e6abe74fa88e4137bd62f1bf99d17fd7
Элина <etatuian262@gmail.com>
2025-07-14 09:17:31 +0300

    Merge pull request #6 from Elina-old/lesson3
    3

---
+++ 05_lesson/lesson05_task2.py
@@ -0,0 +1,15 @@
+
+
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+driver.get("http://uitestingplayground.com/dynamicid")
+sleep(6)
+
+driver.find_element(By.CSS_SELECTOR,"button.btn.btn-primary").click()
+sleep(5)
+
+#driver.quit()
---
+++ 05_lesson/lesson05_task3.py
@@ -0,0 +1,25 @@
+
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Firefox()
+
+driver.get("http://the-internet.herokuapp.com/inputs")
+sleep(3)
+
+search_box = driver.find_element(By.CSS_SELECTOR, '[type="number"]')
+search_box.send_keys("Sky")
+search_box.send_keys(Keys.RETURN)
+sleep(5)
+search_box.clear()
+
+
+sleep(3)
+
+search_box = driver.find_element(By.CSS_SELECTOR, '[type="number"]')
+search_box.send_keys("Pro")
+search_box.send_keys(Keys.RETURN)
+sleep(5)
+driver.quit()
